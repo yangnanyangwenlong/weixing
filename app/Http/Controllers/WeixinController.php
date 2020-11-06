ac83f1ea81573fd24d89af956e1d17cce7eb73d3
yangnan <2589495129@qq.com>
2020-11-06 10:30:28 +0800

    测试1

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -26,24 +26,24 @@ class WeixinController extends Controller
 	//         return false;
 	//     }
 	// }
-	//接口测试
-	public function wx(){
-	    $signature = $_GET["signature"];
-	    $timestamp = $_GET["timestamp"];
-	    $nonce = $_GET["nonce"];
-		
-	    $token = env('WX_TOKEN');
-	    $tmpArr = array($token, $timestamp, $nonce);
-	    sort($tmpArr, SORT_STRING);
-	    $tmpStr = implode( $tmpArr );
-	    $tmpStr = sha1( $tmpStr );
-	    
-	    if( $tmpStr == $signature ){
-	        echo $_GET['echostr'];
-	    }else{
-	        echo '123';
-	    }		
-	}
+	//接口测试/
+		public function wx(){
+		    $signature = $_GET["signature"];
+		    $timestamp = $_GET["timestamp"];
+		    $nonce = $_GET["nonce"];
+			
+		    $token = env('WX_TOKEN');
+		    $tmpArr = array($token, $timestamp, $nonce);
+		    sort($tmpArr, SORT_STRING);
+		    $tmpStr = implode( $tmpArr );
+		    $tmpStr = sha1( $tmpStr );
+		    
+		    if( $tmpStr == $signature ){
+		        echo $_GET['echostr'];
+		    }else{
+		        echo '123';
+		    }		
+		}
 	//接口配置
 	private function checkSignature()
 	{
