0188dad17dc90c0ce960766526e62550ab044fd8
yangnan <2589495129@qq.com>
2020-11-06 10:13:54 +0800

    接口测试5

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -28,10 +28,21 @@ class WeixinController extends Controller
 	// }
 	//接口测试
 	public function wx(){
-		$token= request()->get("echostr");
-		if(!empty($token) && $this->checkSignature()){
-			echo $token;
-		}
+	    $signature = $_GET["signature"];
+	    $timestamp = $_GET["timestamp"];
+	    $nonce = $_GET["nonce"];
+		
+	    $token = env('WX_TOKEN');
+	    $tmpArr = array($token, $timestamp, $nonce);
+	    sort($tmpArr, SORT_STRING);
+	    $tmpStr = implode( $tmpArr );
+	    $tmpStr = sha1( $tmpStr );
+	    
+	    if( $tmpStr == $signature ){
+	        echo $token;
+	    }else{
+	        echo 123;
+	    }		
 	}
 	//接口配置
 	private function checkSignature()
