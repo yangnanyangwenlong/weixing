543d23e857690bd735449c609c19e70848ddb83d
yangnan <2589495129@qq.com>
2020-11-09 13:48:32 +0800

    ee

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
-use app\Model\OpenModel;
+use App\Model\OpenModel;
 class WeixinController extends Controller
 {
 	
