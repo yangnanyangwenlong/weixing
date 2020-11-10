fa58d214b01e8be3d7a61592830ad8014847f966
yangnan <2589495129@qq.com>
2020-11-10 16:46:09 +0800

    wewew

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,25 @@ Route::get('/', function () {
 //练习
 
 Route::prefix('weixin')->group(function(){
-	//Route::get('/Token','test\TestController@token');//测试接入
-	Route::post('/wx','WeixinController@wxEvent');//测试接入、
-	Route::get('/token',"WeixinController@getAccressToken");//获取access_token
-	Route::get('/create_menu','WeixinController@create_menu');//添加菜单
+	//微信开发者服务器接入(即支持get又支持post)
+	Route::match(['get','post'],'/wx','WeixinController@checkSignature');
+	//上传素材
+	Route::get('/guzzle2','WeixinController@guzzle2');
+	//获取access_token
+	Route::get('/access_token','WeixinController@access_token');
+	//天气(780)
+	Route::get('/weather1','WeixinController@weather1');
+	//自定义菜单
+	Route::get('/create_menu','WeixinController@create_menu');
 
- 
+
+	//测试1
+	Route::get('/weather','WeixinController@weather');
+	//测试2
+	Route::get('/test','WeixinController@test');
+	//测试3(postman)
+	Route::get('test2','WeixinController@test2');//get
+	Route::post('test3','WeixinController@test3');//post(form-data)
+	Route::post('test4','WeixinController@test4');//post(raw)
 });
 	
