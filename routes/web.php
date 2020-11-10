18bfc02f9cec40b050de936af24408d547b25c7a
yangnan <2589495129@qq.com>
2020-11-10 13:57:59 +0800

    ss

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::prefix('yangnan')->group(function(){
-// 	Route::get('/','YangnanController@index');
-// 	Route::get('/table','YangnanController@table');
-// });
-// //练习
+Route::prefix('yangnan')->group(function(){
+	Route::get('/','YangnanController@index');
+	Route::get('/table','YangnanController@table');
+});
+//练习
 
 Route::prefix('weixin')->group(function(){
-	Route::match(['get','post'],'wx','WeixinController@wx');//事件（推送
+	Route::post('/wx','weixinController@checkSignature');  //接口微信
+	Route::get('/wx/token','weixinController@token');  //access_token
+	//Route::get('/tell','weixinController@tell');  //postman测试
+	//Route::post('/tell2','weixinController@tell2');  //postman测试
+	Route::get('/custom','weixinController@custom');  //自定义菜单
 
-	// 获取 access_token
-	Route::get('/access_token','WeixinController@access_token');
-	// 测试
-	Route::get('/test','WeixinController@test');
-
- //    Route::get('/turing','WeixinController@turing');
- //    // 测试
- //    Route::get('/test','WeixinController@test');
-	// // 新增临时素材
-	// Route::get('/media/insert','WeixinController@media_insert');
+	//TEST 路由分组
+	//Route::prefix('/text')get()->group(function (){
+	//
+	//});
+	Route::get('getweather','weixinController@getweather');
+	Route::get('/guzzle',"weixinController@guzzle");  //guzzle 测试  GET
+	Route::get('/guzzle2',"weixinController@guzzle2");  //guzzle 测试  POST
 });
 
